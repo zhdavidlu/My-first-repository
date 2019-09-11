f374560f556a049c7abd97eb19436cf4dce8a34b
HEX <25873057+hexiaochuan1@users.noreply.github.com>
2019-09-10 23:55:01 -0700

    Uploaded hash generator
    broken

---
+++ ShashankRedemption.py
@@ -0,0 +1,16 @@
+import hashlib
+import os
+wkdir = os.path.abspath(os.path.dirname(__file__))
+
+whereToThonk = wkdir + "/ + raw_input("Filename: ")
+thonkPower = int(raw_input("Bytes to Read: "))
+
+turnk = hashlib.sha256()
+
+with open(whereToThonk,'rb') as file:
+    thonk = 0
+    while thonk != b'':
+        thonk = file.read(thonkPower)
+        turnk.update(thonk)
+SHA1result = turnk.hexdigest()
+print(SHA1result)
